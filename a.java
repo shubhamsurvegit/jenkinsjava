fa28f425c3f6b26528f6c310e0d1e585c184262a
survegitdemo <surveshubham10@bvcoenm.edu.in>
2022-12-30 03:58:14 +0530

    ff

--- a.java
+++ a.java
@@ -1,12 +1,8 @@
 public class a{
 	public static void main(String[] args){
 	int a=Integer.parseInt(args[0]);	
-<<<<<<< HEAD
-	System.out.println(a*10*4);
-=======
 	System.out.println(a*10*2);
 	System.out.println("f");
->>>>>>> e9efd15c8d25b1371f8e705078f7f58a74185e11
 
 	}
 }
